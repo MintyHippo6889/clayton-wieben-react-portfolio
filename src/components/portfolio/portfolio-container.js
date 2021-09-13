05eef9516050a4e704775b0a6fb8a9a6206c2575
Minty Hippo <clayton.wieben@gmail.com>
2021-09-12 22:35:56 -0600

    added map function to portfolio-container

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,15 +3,26 @@ import React, { Component } from "react";
 import PortfolioItem from './portfolio-item';
 
 export default class PortfolioContainer extends Component {
-    constructor {
+    constructor() {
         super();
+
+        console.log("Portfolio container has rendered");
     }
+
+    portfolioItems() {
+        const data = ["Quip", "Eventbrite", "Ministry Safe"];
+
+        return data.map(item => {
+            return <h1>{item}</h1>;
+        })
+    }
+
     render() {
         return(
             <div>
                 <h2>Portfolio items go here...</h2>
 
-                <portfolioItem />
+                {this.portfolioItems()}
             </div>
         );
     }
