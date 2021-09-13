99c5febdfc5398f02f0fe6e87f8e4018173b9437
Minty Hippo <clayton.wieben@gmail.com>
2021-09-12 22:20:09 -0600

    added "portfolio-item" and "portfolio" folder

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
+import PortfolioItem from './portfolio-item';
+
 export default class PortfolioContainer extends Component {
+    constructor {
+        super();
+    }
     render() {
         return(
             <div>
                 <h2>Portfolio items go here...</h2>
+
+                <portfolioItem />
             </div>
         );
     }
