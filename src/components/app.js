8cf344944e3becc7d77c9202992f54525a903f88
Minty Hippo <clayton.wieben@gmail.com>
2021-10-10 21:02:07 -0600

    added dynamic URLs

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
 
 export default class App extends Component {
   render() {
@@ -15,6 +15,8 @@ export default class App extends Component {
       <div className="app">
         <Router>
           <div>
+            <h1>Clayton Wieben Portfolio</h1>
+            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
             <NavigationContainer />
 
             <Switch>
@@ -22,13 +24,14 @@ export default class App extends Component {
               <Route exact path="/about-me" component={About} />
               <Route exact path="/contact" component={Contact} />
               <Route exact path="/blog" component={Blog} />
+              <Route
+                exact
+                path="/portfolio/:slug"
+                component={PortfolioDetail}
+              />
             </Switch>
           </div>
         </Router>
-
-        <h1>Clayton Wieben Portfolio</h1>
-        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
-        <PortfolioContainer />
       </div>
     );
   }
