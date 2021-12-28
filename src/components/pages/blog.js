7d3bb55b4e1035770c77c53387df0408c0f4b1a4
Minty Hippo <clayton.wieben@gmail.com>
2021-12-28 16:16:39 -0700

    added basic code for blog API

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default function () {
-  return (
-    <div>
-      <h2>Blog</h2>
+class Blog extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      blogItems: [],
+    };
+
+    this.getBlogItems = this.geBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://claytonwieben.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log("response", response);
+      })
+      .catch((error) => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    return (
       <div>
-        <Link to="/about-me">Read more about my work</Link>
+        <h2>Blog</h2>
+
+        <div>
+          <Link to="/about-me">Read more about my work</Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Blog;
