b02bd132b43de8edfa9849f0d405a659c2a1a4af
Minty Hippo <clayton.wieben@gmail.com>
2022-02-23 22:17:08 -0700

    added content and styling to About and Contact pages

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,36 @@
 import React from "react";
+import profilePicture from "../../../static/assets/images/bio/Headshot.jpg";
 
 export default function () {
-  return <div>About</div>;
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + profilePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="right-column">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero et
+        doloribus quasi necessitatibus vero aliquam eaque dolorem impedit. Sed
+        in veritatis sunt eaque, reiciendis a perferendis qui! Nisi, deserunt?
+        Reiciendis beatae est ex aut porro illo nesciunt fugit laborum autem
+        vel, voluptatem rerum magni ducimus nobis quos architecto quisquam
+        distinctio itaque, impedit ipsa vitae facere omnis sit temporibus.
+        Cupiditate a voluptas repudiandae minima. Sed omnis autem a excepturi
+        reprehenderit commodi blanditiis! Eligendi vero totam est sint incidunt
+        consequatur id, eveniet doloribus blanditiis perferendis repellat
+        perspiciatis ullam, aperiam autem? Exercitationem error mollitia ipsum
+        quae sequi atque voluptatum, ad expedita reprehenderit id a, quasi
+        officia harum ex assumenda! Quae exercitationem magnam, perspiciatis
+        nisi commodi ipsa mollitia ex rerum, fugiat ducimus ea asperiores quis
+        repudiandae totam, provident optio aperiam ipsum? Corporis iste veniam
+        modi dolore aliquid nam omnis, asperiores, nesciunt doloribus in
+        mollitia nihil dolorum corrupti suscipit ex deleniti tempore excepturi
+        labore quaerat.
+      </div>
+    </div>
+  );
 }
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,5 +1,45 @@
 import React from "react";
+import contactPicture from "../../../static/assets/images/bio/Contact_Me.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function () {
-  return <div>Contact</div>;
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + contactPicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="right-column">
+        <div className="contact-bullet-points">
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="phone" />
+            </div>
+
+            <div className="text">208-569-3307</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="envelope" />
+            </div>
+
+            <div className="text">Clayton.Wieben@gmail.com</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="map-marked-alt" />
+            </div>
+
+            <div className="text">Rexburg, ID</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
